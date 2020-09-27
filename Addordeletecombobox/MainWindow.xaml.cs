8eed20858c3f0d38b890c2546f3b9ca7821d076f
jovenbugay <jovenbugay@gmail.com>
2020-09-27 16:35:59 +0800

    update upto 09.27.2020

---
+++ Addordeletecombobox/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Addordeletecombobox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Resource/Coffee.png"));
+            img.Height = 20;
+
+            TextBlock txt = new TextBlock();
+            txt.Inlines.Add(TextBox.Text);
+
+            StackPanel stackpanel = new StackPanel();
+            stackpanel.Orientation = Orientation.Horizontal;
+
+            stackpanel.Children.Add(img);
+            stackpanel.Children.Add(txt);
+
+            ComboBox1.Items.Add(stackpanel);
+            TextBox.Text = "";
+
+
+        }
+        private void RemoveButton_CLick(object sender, RoutedEventArgs e)
+        {
+            ComboBox1.Items.RemoveAt(ComboBox1.Items.IndexOf(ComboBox1.SelectedItem));
+        }
+    }
+}
